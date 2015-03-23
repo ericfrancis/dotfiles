788bb69d00f29996552bd1c76372544082e121c2
eric francis <ericf@slalom.com>
2015-03-23 11:09:37 -0500

    installs git submodules

--- bin/provision.sh
+++ bin/provision.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-sudo apt-get install vim-gtk curl wget tree git-flow
 
-# tmux needs version 1.9
+echo tmux needs version 1.9
 sudo add-apt-repository ppa:pi-rho/dev
-sudo apt-get update
-sudo apt-get install tmux 
 
-# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+sudo apt-get update
+sudo apt-get install tmux vim-gtk curl wget tree git-flow
+
+echo installing git submodules
+git submodule init
+git submodule update
