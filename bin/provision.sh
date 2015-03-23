0925c8d915cd0ba3052e9f3e9f8963da0f5bedcb
eric francis <ericf@slalom.com>
2015-03-23 09:46:50 -0500

    visual changes

--- bin/provision.sh
+++ bin/provision.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 sudo apt-get install vim-gtk curl wget tree git-flow
 
 # tmux needs version 1.9
@@ -8,11 +7,11 @@ sudo apt-get update
 sudo apt-get install tmux 
 
 
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
-cd ~/.vim/bundle && \ 
-	git clone git://github.com/tpope/vim-sensible.git && \
-	git clone https://github.com/christoomey/vim-tmux-navigator.git
-
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+# 	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+# 
+# cd ~/.vim/bundle && \ 
+# 	git clone git://github.com/tpope/vim-sensible.git && \
+# 	git clone https://github.com/christoomey/vim-tmux-navigator.git
+# 
+# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
